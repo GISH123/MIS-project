c37a0fc64b9bbe45f893ebaeab76a9c9c4b0cc07
SSARCandy <ssarcandy@gmail.com>
2015-10-12 01:41:43 +0800

    update

--- js/force_direct.js
+++ js/force_direct.js
@@ -13,13 +13,56 @@ var nodes_ori = [],
 	links_branch = [];
 
 function activate() {
-	d3.json("./data/100.json", function(error, graph) {
+	// define arrow markers for graph links
+    d3.select("svg")
+			.append("defs").append("marker")
+            .attr("id", "arrow-branch")
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", 10)
+            .attr("refY", 0)
+            .attr("markerWidth", 5)
+            .attr("markerHeight", 5)
+            .attr("orient", "auto")
+            .append("path")
+            .attr("fill", "#0FF")
+            .attr("stroke", "#0FF")
+            .attr("stroke-width", "0.1px")
+            .attr("d", "M0,-5L10,0L0,5Z");
+
+    d3.select("defs").append("marker")
+            .attr("id", "arrow-trade")
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", 10)
+            .attr("refY", 0)
+            .attr("markerWidth", 5)
+            .attr("markerHeight", 5)
+            .attr("orient", "auto")
+            .append("path")
+            .attr("fill", "#000")
+            .attr("stroke", "#000")
+            .attr("stroke-width", "0.1px")
+            .attr("d", "M0,-5L10,0L0,5Z");
+
+    d3.select("defs").append("marker")
+            .attr("id", "tail-crossed")
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", -35)
+            .attr("refY", 0)
+            .attr("markerWidth", 8)
+            .attr("markerHeight", 8)
+            .attr("orient", "auto")
+            .append("path")
+            .attr("class", "link")
+            .attr("d", "M0,-5L0,5");
+	
+	
+	d3.json("./data/100.json", function (error, graph) {
 		if (error) return;
 
 		var types = [],
 			nodeHash = {};
 		// assign vertices to nodes array
-		graph.vertices.forEach(function(vertex, i) {
+		graph.vertices.forEach(function (vertex, i) {
 			nodeHash[vertex._id] = i;
 
 			nodes_ori.push({
@@ -44,10 +87,11 @@ function activate() {
 		});
 
 		// assign edges to links array
-		graph.edges.forEach(function(edge, i) {
-			if(edge._label === '分支機構 ') {
+		graph.edges.forEach(function (edge, i) {
+			if (edge._label === '分支機構 ') {
 				links_branch.push({
 					id: i.toString(),
+					attributes: edge,
 					source: nodeHash[edge._outV].toString(),
 					target: nodeHash[edge._inV].toString(),
 					label: edge._label,
@@ -56,6 +100,7 @@ function activate() {
 			} else {
 				links_trade.push({
 					id: i.toString(),
+					attributes: edge,
 					source: nodeHash[edge._outV].toString(),
 					target: nodeHash[edge._inV].toString(),
 					label: edge._label,
@@ -75,32 +120,39 @@ function activate() {
 			.on("tick", redrawGraph);
 
 		zoom = d3.behavior.zoom()
-			.scaleExtent([0.01, 1])
+			.scaleExtent([0.01, 2])
 			.on("zoom", zoomed).scale(0.02);
 
 		allLinks = gD3.links();
 
-		d3.select("svg").call(zoom).attr("transform","scale(.02,.02)");
+		d3.select("svg").call(zoom).attr("transform", "scale(.02,.02)");
 		zoomed();
 		draw();
+		force.start();
+		// console.log(gD3.links());
 	});
 }
 
 function loadGraph(show) {
 	var newGEXF = GexfParser.fetch('./data/lm.gexf');
-	
+
 	d3.selectAll("svg > g > *").remove();
 
-	newGEXF.edges = show === 'branch' ? links_branch : links_trade;
 	newGEXF.nodes = nodes_ori;
-	console.log(newGEXF.nodes);
 	
-	if(!gD3) {
+	if(show === 'branch') { newGEXF.edges = links_branch; }
+	else if(show === 'trade') { newGEXF.edges = links_trade; }
+	else { newGEXF.edges = links_branch.concat(links_trade); }
+
+
+	// console.log(newGEXF.nodes);
+	
+	if (!gD3) {
 		gD3 = gexfD3().graph(newGEXF).size([1000, 1000]).nodeScale([5, 20]);
 		//console.log(gD3.nodes());
 	} else {
-		newGEXF.nodes = gD3.nodes().map(function(n, i) {
-			return { 
+		newGEXF.nodes = gD3.nodes().map(function (n, i) {
+			return {
 				id: i.toString(),
 				name: n.label,
 				label: n.label,
@@ -114,17 +166,18 @@ function loadGraph(show) {
 						z: 0
 					}
 				}
-			};			
+			};
 		});
-	console.log(newGEXF.nodes);
+		// console.log(newGEXF.nodes);
 		gD3 = gexfD3().graph(newGEXF).size([1000, 1000]).nodeScale([5, 20]);
 	}
+
 }
 
 function highlightNeighbors(d, i) {
 	var nodeNeighbors = findNeighbors(d, i);
-	
-	d3.selectAll("g.node").each(function(p) {
+
+	d3.selectAll("g.node").each(function (p) {
 		var isNeighbor = nodeNeighbors.nodes.indexOf(p);
 		d3.select(this).select("circle")
 			.style("opacity", isNeighbor > -1 ? 1 : 0.25)
@@ -133,10 +186,7 @@ function highlightNeighbors(d, i) {
 	});
 
 	d3.selectAll("line.link")
-		.style("stroke-width", function(d) {
-			return nodeNeighbors.links.indexOf(d) > -1 ? 2 : 1;
-		})
-		.style("opacity", function(d) {
+		.style("opacity", function (d) {
 			return nodeNeighbors.links.indexOf(d) > -1 ? 1 : 0.25;
 		});
 }
@@ -144,14 +194,14 @@ function highlightNeighbors(d, i) {
 function findNeighbors(d, i) {
 	neighborArray = [d];
 	var linkArray = [];
-	var linksArray = d3.selectAll("line.link").filter(function(p) {
-			return p.source == d || p.target == d;
-		}).each(function(p) {
-			neighborArray.indexOf(p.source) == -1 ? neighborArray.push(p.source) : null;
-			neighborArray.indexOf(p.target) == -1 ? neighborArray.push(p.target) : null;
-			linkArray.push(p);
-		});
-		//        neighborArray = d3.set(neighborArray).keys();
+	var linksArray = d3.selectAll("line.link").filter(function (p) {
+		return p.source == d || p.target == d;
+	}).each(function (p) {
+		neighborArray.indexOf(p.source) == -1 ? neighborArray.push(p.source) : null;
+		neighborArray.indexOf(p.target) == -1 ? neighborArray.push(p.target) : null;
+		linkArray.push(p);
+	});
+	//        neighborArray = d3.set(neighborArray).keys();
 	return {
 		nodes: neighborArray,
 		links: linkArray
@@ -159,10 +209,12 @@ function findNeighbors(d, i) {
 }
 
 function zoomed() {
+	console.log(zoom.scale());
 	force.stop();
 	var canvWidth = parseInt(d3.select("#vizcontainer").style("width"));
 	var canvHeight = parseInt(d3.select("#vizcontainer").style("height"));
 	if (currentScale != zoom.scale()) {
+
 		currentScale = zoom.scale();
 		var halfCanvas = canvHeight / 2;
 		var zoomLevel = halfCanvas * currentScale;
@@ -174,19 +226,19 @@ function zoomed() {
 	}
 	var canvasTranslate = zoom.translate();
 	d3.select("#graphG").attr("transform", "translate(" + canvasTranslate[0] + "," + canvasTranslate[1] + ")");
-		// force.start();
+	// force.start();
 }
 
 function createControls() {
-	d3.select("#controls").append("button").attr("class", "origButton").html("Force On").on("click", function() {
+	d3.select("#controls").append("button").attr("class", "origButton").html("Force On").on("click", function () {
 		force.start();
 	});
-	d3.select("#controls").append("button").attr("class", "origButton").html("Force Off").on("click", function() {
+	d3.select("#controls").append("button").attr("class", "origButton").html("Force Off").on("click", function () {
 		force.stop();
 	});
-	d3.select("#controls").append("button").attr("class", "origButton").html("Reset Layout").on("click", function() {
+	d3.select("#controls").append("button").attr("class", "origButton").html("Reset Layout").on("click", function () {
 		force.stop();
-		gD3.nodes().forEach(function(el) {
+		gD3.nodes().forEach(function (el) {
 			el.x = el.originalX;
 			el.px = el.originalX;
 			el.y = el.originalY;
@@ -196,72 +248,89 @@ function createControls() {
 		draw();
 		redrawGraph();
 	});
-	d3.select("#controls").append("button").attr("class", "origButton").html("顯示分支機構關係").on("click", function() {
+	d3.select("#controls").append("button").attr("class", "origButton").html("顯示分支機構關係").on("click", function () {
 		force.stop();
 		loadGraph('branch');
 		currentBrush = [0, 0];
-		//zoomed();
+		force = d3.layout.force()
+			.charge(-1)
+			.linkDistance(1)
+			.size([1, 1])
+			.gravity(0.1)
+			.on("tick", redrawGraph);
+
+		zoom = d3.behavior.zoom()
+			.scaleExtent([0.01, 3])
+			.on("zoom", zoomed).scale(0.6);
+
+		allLinks = gD3.links();
+
+		d3.select("svg").call(zoom).attr("transform", "scale(.6,.6)");
+		zoomed();
 		draw();
-		force.start();
+
 	});
-	d3.select("#controls").append("button").attr("class", "origButton").html("顯示交易關係").on("click", function() {
+	d3.select("#controls").append("button").attr("class", "origButton").html("顯示交易關係").on("click", function () {
 		force.stop();
 		loadGraph('trade');
 		currentBrush = [0, 0];
-		//zoomed();
+		force = d3.layout.force()
+			.charge(-1)
+			.linkDistance(1)
+			.size([1, 1])
+			.gravity(0.1)
+			.on("tick", redrawGraph);
+
+		zoom = d3.behavior.zoom()
+			.scaleExtent([0.01, 3])
+			.on("zoom", zoomed).scale(0.6);
+
+		allLinks = gD3.links();
+
+		d3.select("svg").call(zoom).attr("transform", "scale(.6,.6)");
+		zoomed();
 		draw();
-		force.start();
 	});
+	d3.select("#controls").append("button").attr("class", "origButton").html("顯示全部").on("click", function () {
+		force.stop();
+		loadGraph('all');
+		currentBrush = [0, 0];
+		force = d3.layout.force()
+			.charge(-1)
+			.linkDistance(1)
+			.size([1, 1])
+			.gravity(0.1)
+			.on("tick", redrawGraph);
 
-	// d3.select("#controls").append("button").attr("class", "origButton").html("Reset Colors").on("click", function() {
-	// 	var sizeScale = gD3.nodeScale();
-	// 	d3.selectAll("circle")
-	// 		.attr("r", function(d) {
-	// 			return sizeScale(d.size)
-	// 		})
-	// 		.style("fill", function(d) {
-	// 			return d.rgbColor
-	// 		})
-	// 		.style("opacity", 1);
-	// 	d3.selectAll("line.link").style("stroke", "black");
-	// })
+		zoom = d3.behavior.zoom()
+			.scaleExtent([0.01, 3])
+			.on("zoom", zoomed).scale(0.6);
 
-	// d3.select("#controls").selectAll("button.nodeButtons").data(gD3.nodeAttributes())
-	// 	.enter()
-	// 	.append("button")
-	// 	.attr("class", "nodeButtons")
-	// 	.on("click", nodeButtonClick)
-	// 	.html(function(d) {
-	// 		return d
-	// 	});
+		allLinks = gD3.links();
 
-	// d3.select("#controls").selectAll("button.linkButtons").data(gD3.linkAttributes())
-	// 	.enter()
-	// 	.append("button")
-	// 	.attr("class", "linkButtons")
-	// 	.on("click", linkButtonClick)
-	// 	.html(function(d) {
-	// 		return d
-	// 	});
+		d3.select("svg").call(zoom).attr("transform", "scale(.6,.6)");
+		zoomed();
+		draw();
+	});
 }
 
 function nodeButtonClick(d, i) {
-	var nodeAttExtent = d3.extent(gD3.nodes(), function(p) {
+	var nodeAttExtent = d3.extent(gD3.nodes(), function (p) {
 		return parseFloat(p.properties[d]);
 	});
 	var colorScale = d3.scale.quantize().domain(nodeAttExtent).range(colorbrewer.YlGnBu[6]);
 	d3.selectAll("circle")
-		.style("fill", function(p) { return colorScale(p.properties[d]); })
+		.style("fill", function (p) { return colorScale(p.properties[d]); })
 		.style("opacity", 1);
 }
 
 function linkButtonClick(d, i) {
-	var linkAttExtent = d3.extent(gD3.links(), function(p) {
+	var linkAttExtent = d3.extent(gD3.links(), function (p) {
 		return parseFloat(p.properties[d]);
 	});
 	var colorScale = d3.scale.quantize().domain(linkAttExtent).range(colorbrewer.YlGnBu[6]);
 	d3.selectAll("line")
-		.style("stroke", function(p) { return colorScale(p.properties[d]); })
+		.style("stroke", function (p) { return colorScale(p.properties[d]); })
 		.style("opacity", 1);
 }
 
@@ -270,18 +339,16 @@ function redrawGraph() {
 	var yScale = gD3.yScale();
 
 	d3.selectAll("line.link")
-		.attr("x1", function(d) { return xScale(d.source.x); })
-		.attr("y1", function(d) { return yScale(d.source.y); })
-		.attr("x2", function(d) { return xScale(d.target.x); })
-		.attr("y2", function(d) { return yScale(d.target.y); });
+		.attr("x1", function (d) { return xScale(d.source.x); })
+		.attr("y1", function (d) { return yScale(d.source.y); })
+		.attr("x2", function (d) { return xScale(d.target.x); })
+		.attr("y2", function (d) { return yScale(d.target.y); })
+		.style("marker-end", function(d) { return d.label === "分支機構 " ? "url(#arrow-branch)" : "url(#arrow-trade)"; });
 
 	d3.selectAll("g.node")
-		.attr("transform", function(d) {
+		.attr("transform", function (d) {
 			return "translate(" + xScale(d.x) + "," + yScale(d.y) + ")";
 		});
-
-	//d3.select("svg").call(zoom).attr("transform","scale(.02,.02)");
-	//zoomed();
 }
 
 function draw() {
@@ -295,41 +362,43 @@ function draw() {
 		forceRunning = true;
 	}
 
-	d3.select("#graphG").selectAll("line.link")
-		.data(gD3.links(), function(d) {
+	d3.select("#graphG")
+		.selectAll("line.link")
+		.data(gD3.links(), function (d) {
 			return d.id;
 		})
 		.enter()
 		.insert("line", "g.node")
 		.attr("class", "link")
-		.attr("x1", function(d) { return xScale(d.source.x); })
-		.attr("x2", function(d) { return xScale(d.target.x); })
-		.attr("y1", function(d) { return yScale(d.source.y); })
-		.attr("y2", function(d) { return yScale(d.target.y); })
-		.style("stroke", function(d) {
-			if(d.label === '分支機構 ') { return 'cyan'; }
+		.attr("x1", function (d) { return xScale(d.source.x); })
+		.attr("x2", function (d) { return xScale(d.target.x); })
+		.attr("y1", function (d) { return yScale(d.source.y); })
+		.attr("y2", function (d) { return yScale(d.target.y); })
+		.on("click", linkClick)
+		.style("stroke", function (d) {
+			if (d.label === '分支機構 ') { return 'cyan'; }
 			else { return 'black'; }
 		})
-		.style("stroke-width", "1px")
+		.style("stroke-width", "2px")
 		.style("opacity", 0.5);
 
 	d3.select("#graphG")
 		.selectAll("g.node")
-		.data(gD3.nodes(), function(d) { return d.id; })
+		.data(gD3.nodes(), function (d) { return d.id; })
 		.enter()
 		.append("g")
 		.attr("class", "node")
-		.attr("transform", function(d) {
+		.attr("transform", function (d) {
 			return "translate(" + xScale(d.x) + "," + yScale(d.y) + ")";
 		})
 		.on("mouseover", nodeOver)
 		.on("mouseout", nodeOut)
 		.on("click", nodeClick)
 		.append("circle")
-		.attr("r", function(d) {
+		.attr("r", function (d) {
 			return findNeighbors(d, 0).nodes.length + 1;
 		})
-		.style("fill", function(d) {
+		.style("fill", function (d) {
 			return d.rgbColor;
 		})
 		.style("stroke", "black")
@@ -348,16 +417,14 @@ function draw() {
 		if (nodeFocus) {
 			return;
 		}
-		//console.log(d,i,e);
 		
 		//Only do the element stuff if this came from mouseover
 		el.parentNode.appendChild(el);
-		
+
 		d3.select(el)
 			.append("text")
 			.attr("class", "hoverLabel")
 			.attr("stroke", "white")
-			.attr("stroke-width", "5px")
 			.style("opacity", 0.9)
 			.style("pointer-events", "none")
 			.text(d.label);
@@ -367,7 +434,7 @@ function draw() {
 			.attr("class", "hoverLabel")
 			.style("pointer-events", "none")
 			.text(d.label);
-			
+
 		highlightNeighbors(d, i);
 	}
 
@@ -376,7 +443,7 @@ function draw() {
 		nodeOut();
 		nodeOver(d, i, this);
 		nodeFocus = true;
-		
+
 		var newContent = "<p>" + d.label + "</p>";
 		newContent += "<p>Attributes: </p><p><ul>";
 		for (x in gD3.nodeAttributes()) {
@@ -393,6 +460,23 @@ function draw() {
 
 		d3.select("#modal").style("display", "block").select("#content").html(newContent);
 	}
+
+	function linkClick(d, i) {
+		nodeFocus = false;
+		nodeOut();
+		nodeFocus = true;
+		
+		var newContent = "<p>" + d.label + "</p>";
+		newContent += "<p>Attributes: </p><p><ul>";
+
+		for (x in gD3.linkAttributes()) {
+			newContent += "<li>" + gD3.linkAttributes()[x] + ": " + d.properties[gD3.linkAttributes()[x]] + "</li>";
+		}
+
+		newContent += "</ul></p>";
+
+		d3.select("#modal").style("display", "block").select("#content").html(newContent);
+	}
 }
 
 function nodeOut() {
@@ -401,6 +485,10 @@ function nodeOut() {
 	}
 
 	d3.selectAll(".hoverLabel").remove();
-	d3.selectAll("circle").style("opacity", 1).style("stroke", "black").style("stroke-width", "1px");
-	d3.selectAll("line").style("opacity", 0.25);
+	d3.selectAll("circle").style("opacity", 1).style("stroke", "black").style("stroke-width", "2px");
+	d3.selectAll("line").style("opacity", 0.8);
 }
+
+    function linkArrow(d) {
+        return "M" + d.source.x + "," + d.source.y + "L" + d.target.x + "," + d.target.y;
+    }
