ea7fc457ca079d3307309a08185dced2f37de45a
wang <wang@ubuntu>
2015-12-06 20:58:42 +0800

    update

--- js/gremlin.js
+++ js/gremlin.js
@@ -9,15 +9,16 @@ $("#gremlin_code_text").keydown(function(e) {
 });
 
 $("#gremlin_code_submit").on("click", function(e) {
+	
 	if ($("#gremlin_code_text").val().trim() != "") {
 		
 		$("#gremlin_result").empty();
 		$("#gremlin_code_submit").attr("disabled", true);
-		$("#gremlin_result_loadingIMG").show();
+		$("#gremlin_result_loading").show();
 
 		var func = getGremlinScript($("#gremlin_graph_list").val(), $("#gremlin_code_text").val());
 		func.done(function(response) {
-			$("#gremlin_result").append(JSON.stringify(response["results"]));
+			$("#gremlin_result").append(JSON.stringify(response["results"], null, "\t"));
 		}).fail(function() {
 			$("#gremlin_result").append("Input wrong.");
 		}).always(function() {
@@ -27,7 +28,7 @@ $("#gremlin_code_submit").on("click", function(e) {
 				"margin-top" : "10px"
 			});
 			$("#gremlin_code_submit").attr("disabled", false);
-			$("#gremlin_result_loadingIMG").hide();
+			$("#gremlin_result_loading").hide();
 		});
 	}
 });
